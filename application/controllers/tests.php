35f010128ed413aa4badaffdf9d6c5cb1cf98fe3
Matthew Carmody <mr.carmody@gmail.com>
2014-05-21 15:39:09 -0700

    cleans up tests.php controller

--- application/controllers/tests.php
+++ application/controllers/tests.php
@@ -3,19 +3,8 @@
 class Tests extends CI_Controller {
 
 	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 * Test Controller
+	 * Runs unit tests
 	 */
 	public function index()
 	{
@@ -89,5 +78,5 @@ class Tests extends CI_Controller {
 	}
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+/* End of file tests.php */
+/* Location: ./application/controllers/tests.php */
